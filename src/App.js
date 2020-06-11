4d4fc1d9377f1c7e7a16176104b522193196d13e
batel-shafir <batyushafir+1@gmail.com>
2020-06-11 20:01:35 +0300

    added css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import CovidDashboard from './Components/CovidDashboard';
 
 function App() {
   return (
     <div className="App">
-     <CovidDashboard/>
+      <CovidDashboard/>
     </div>
   );
 }
--- src/Components/CovidDashboard.js
+++ src/Components/CovidDashboard.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { CircularProgress } from '@material-ui/core';
+import './CovidDashboard.css';
+
  
 export async function getNumberByCountry (country, code) {
     const firstDay =  await axios.get(`https://api.covid19api.com/country/${country}/status/confirmed?from=2020-03-11T00:00:00Z&to=2020-03-11T23:59:59Z`);
@@ -22,45 +24,53 @@ function CovidDashboard(props) {
             const response = await axios.get('https://api.covid19api.com/summary');
             const countriesNames = response.data.Countries.map(country => {return({name:country.Slug, code:country.CountryCode.toLowerCase() })});
             console.log(countriesNames);
-            setCountries(countriesNames.filter(country => country.name != 'united-states'));
+            setCountries(countriesNames.filter(country => country.name !== 'united-states'));
         };
         fetchCountries().then(null);
     }, []);
   
    
     useEffect(() => {
-        async function fetchAmounts() {
-            const arr = await Promise.all(countries.map(country => getNumberByCountry(country.name,country.code)));
-            const sortedArr = arr.sort((a, b) => {
+        const gotCountriesNames = countries.length > 0;
+
+        async function fetchNumberOfCases() {
+            const countriesWithConfirmedCases = await Promise.all(countries.map(country => getNumberByCountry(country.name,country.code)));
+            const sortedArr = countriesWithConfirmedCases.sort((a, b) => {
                 return b.confirmedCases - a.confirmedCases;
             });
-            const top10Confirmed = sortedArr.slice(0,10);
-            setTop10Countries(top10Confirmed);
-           
+            const top10ConfirmedCountries = sortedArr.slice(0,10);
+            setTop10Countries(top10ConfirmedCountries);   
         }
-        if (countries.length > 0) {
-            fetchAmounts().then(null);
-        }
+        
+        gotCountriesNames && fetchNumberOfCases().then(null);
+        
     },[countries]
     );
 
-  
 
-  
   return (
     <div className="data-container">           
-        {isLoading ? 
+        {isLoading && 
             <div className="centered">
-            <CircularProgress></CircularProgress>
-            </div> :
-        top10Countries.map(x => {
-            return (
-                <div>
-                    <div>{ x.name }</div>
-                    <div>{ x.confirmedCases }</div>
-                    <img src={`https://www.countryflags.io/${x.code}/flat/64.png`}/>
-                </div>) 
-        })}         
+                <div className="loading-header">Fetching the data...</div>
+                <CircularProgress></CircularProgress>
+        </div>}
+        {!isLoading &&
+         
+        <div className="countries-container">
+            <div className="countries-header">
+                10 countries with highest amount of confirmed cases in the past 3 months
+            </div>
+            {top10Countries.map(country => {
+                return (     
+                    <div className="country-info">
+                        <div>{ country.name }</div>
+                        <div>{ country.confirmedCases }</div>
+                        <img alt="flag" src={`https://www.countryflags.io/${country.code}/flat/64.png`}/>
+                    </div>)
+                
+            })}  
+        </div> }     
         </div>
     
     
